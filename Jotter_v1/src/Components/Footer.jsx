d5b7344942b34c3efdbaf9da90f4ca73d0363768
Kartik Khanna <kartikkhanna2000@gmail.com>
2021-08-03 14:27:43 +0530

    Files Added

---
+++ Jotter_v1/src/Components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Heading(){
+    const date=new Date().getFullYear();
+    return <footer><p>Copyright &copy; {date}</p></footer>;
+}
+export default Heading;
---
+++ Jotter_v1/src/Components/Header.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Heading(){
+    return <header><h1>Jotter</h1></header>;
+}
+export default Heading;
