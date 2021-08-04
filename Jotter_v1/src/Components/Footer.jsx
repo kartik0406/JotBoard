de6c8af6c08b4dede98cf08a0d742750482670c9
Kartik Khanna <66087752+kartik0406@users.noreply.github.com>
2021-08-04 22:34:41 +0530

    Delete Jotter_v1 directory

--- Jotter_v1/src/Components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Heading(){
-    const date=new Date().getFullYear();
-    return <footer><p>Copyright &copy; {date}</p></footer>;
-}
-export default Heading;
--- Jotter_v1/src/Components/Header.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function Heading() {
-    return <header><h1>Jotter</h1></header>;
-}
-export default Heading;
